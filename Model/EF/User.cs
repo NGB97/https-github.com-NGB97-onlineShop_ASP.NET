8c56317875a9f8882e1fa64bd3d9952ad2fe23ed
Nguyễn Gia Bảo <redromany121@gmail.com>
2019-03-20 23:40:50 +0700

    Thêm Tài Khoản

--- Model/EF/User.cs
+++ Model/EF/User.cs
@@ -1,4 +1,4 @@
-namespace Model.EF
+﻿namespace Model.EF
 {
     using System;
     using System.Collections.Generic;
@@ -12,24 +12,29 @@ namespace Model.EF
         public long ID { get; set; }
 
         [StringLength(50)]
+        [Display(Name="Tài Khoản")]
         public string UserName { get; set; }
 
         [StringLength(32)]
+        [Display(Name = "Mật Khẩu")]
         public string Password { get; set; }
 
         [StringLength(20)]
         public string GroupID { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Họ Tên")]
         public string Name { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Địa Chĩ")]
         public string Address { get; set; }
 
         [StringLength(50)]
         public string Email { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Số Điện Thoại")]
         public string Phone { get; set; }
 
         public int? ProvinceID { get; set; }
@@ -46,6 +51,7 @@ namespace Model.EF
         [StringLength(50)]
         public string ModifiedBy { get; set; }
 
+        [Display(Name = "Trạng Thái")]
         public bool Status { get; set; }
     }
 }
---
+++ OnlineShop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Model.DAO;
+using Model.EF;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var encryptedMd5Pas = Encryptor.MD5Hash(user.Password);
+                user.Password = encryptedMd5Pas;
+                long id = dao.Insert(user);
+                if (id > 0)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm user thành công");
+                }
+            }
+            return View("Index");
+        }
+        
+    }
+}
